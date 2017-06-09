5a2483c8ab6a7e300cb0604624f170aa532f3017
Snaacky <Snaacky@users.noreply.github.com>
2017-06-09 09:40:05 -0400

    Cleaned up code
    pep8 and all that good stuff

--- banana_split.py
+++ banana_split.py
@@ -7,18 +7,23 @@ client = SteamClient()
 @client.on("logged_on")
 def handle_after_logon():
     print("Logged on as: {0}".format(client.user.name))
-    client.change_status(persona_state=EPersonaState.Online, player_name=client.user.name, persona_state_flags=EPersonaStateFlag.HasGoldenProfile)
+    client.change_status(persona_state=EPersonaState.Online,
+                         player_name=client.user.name,
+                         persona_state_flags=EPersonaStateFlag.HasGoldenProfile)
     print("Press ^C to exit")
 
 
-try:
-    result = client.cli_login()
+def main():
+    try:
+        result = client.cli_login()
+        if result != EResult.OK:
+            print("Failed to login: {0}".format(repr(result)))
+            raise SystemExit
+        client.run_forever()
+    except KeyboardInterrupt:
+        if client.connected:
+            client.logout()
 
-    if result != EResult.OK:
-        print("Failed to login: {0}".format(repr(result)))
-        raise SystemExit
 
-    client.run_forever()
-except KeyboardInterrupt:
-    if client.connected:
-        client.logout()
+if __name__ == '__main__':
+    main()
